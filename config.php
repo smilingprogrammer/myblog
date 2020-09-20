0fe929bb292a0e95365339e870970eccd3f28c6a
smilingprogrammer <66224527+smilingprogrammer@users.noreply.github.com>
2020-09-20 22:50:16 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 	session_start();
 
 	// connect to database
-    $conn = mysqli_connect("localhost", "root", "123456", "blog");
+    $conn = mysqli_connect("us-cdbr-east-02.cleardb.com", "b873ff23d283c2", "a8f73ae7", "heroku_db342da0589c870");
 
     if (!$conn) {
     	die("Failed database connection" . mysqli_connect_error());
@@ -11,6 +11,6 @@
 
 
 	define ('ROOT_PATH', realpath(dirname(__FILE__)));
-	define('BASE_URL', 'http://localhost/blog/');
+	define('BASE_URL', 'mysql://b873ff23d283c2:a8f73ae7@us-cdbr-east-02.cleardb.com/heroku_db342da0589c870?reconnect=true');
 ?>
 
